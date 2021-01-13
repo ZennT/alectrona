a7da28bb41d75aee149da6296869f932910fadc2
oturkoz <cinaralti80>
2021-01-13 07:03:31 -0800

    asking for age

--- src/com/cb/assignment_1.java
+++ src/com/cb/assignment_1.java
@@ -1,8 +1,13 @@
 package com.cb;
 
+import java.util.Scanner;
+
 public class assignment_1 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How old are you?");
+        int number = scanner.nextInt();
 
     }
 }
