fd15ca2c0dd41930b31dfc0420dedca59b68043f
oturkoz <cinaralti80>
2021-01-16 01:46:41 -0800

    asking for age continuously in a while loop

--- src/com/cb/assignment_1.java
+++ src/com/cb/assignment_1.java
@@ -6,8 +6,25 @@ public class assignment_1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How old are you?");
-        int number = scanner.nextInt();
 
+        int age = 1;
+
+        while(age > 0)
+        {
+            System.out.println("How old are you?");
+            age = scanner.nextInt();
+
+            if(13 > age && 0 < age) {
+                System.out.println("This movie contains violence and not appropriate for you.");
+            }
+
+            else if(13 <= age && 18 > age) {
+                System.out.println("This movie contains nudity and not appropriate for you.");
+            }
+
+            else if (18 <= age) {
+                System.out.println("This movie is appropriate for you.");
+            }
+        }
     }
 }
