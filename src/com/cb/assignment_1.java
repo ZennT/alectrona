341d41c13c847a14b1fd2a9d91919ab8c152b1d8
oturkoz <cinaralti80>
2021-01-16 23:27:33 -0800

    update

--- src/com/cb/assignment_1.java
+++ src/com/cb/assignment_1.java
@@ -7,18 +7,19 @@ public class assignment_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int age = 1;
+        int age = 1; // while loop a girmesi icin age 0 dan buyuk herhangi bir deger olabilir
 
-        while(age > 0)
-        {
+        while(age > 0) {
             System.out.println("How old are you?");
             age = scanner.nextInt();
 
-            if(13 > age && 0 < age) {
+//            if(13 > age && 0 < age) {
+            if(0 < age && age < 13) {
+
                 System.out.println("This movie contains violence and not appropriate for you.");
             }
 
-            else if(13 <= age && 18 > age) {
+            else if(13 <= age && age < 18) {
                 System.out.println("This movie contains nudity and not appropriate for you.");
             }
 
