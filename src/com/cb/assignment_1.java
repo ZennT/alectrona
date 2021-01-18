b9ab46d068981eb92fb4b52c4170ecacdb4cf28c
oturkoz <cinaralti80>
2021-01-17 19:34:17 -0800

    added a new solution to assignment_1

--- src/com/cb/assignment_1.java
+++ src/com/cb/assignment_1.java
@@ -7,13 +7,14 @@ public class assignment_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int age = 1; // while loop a girmesi icin age 0 dan buyuk herhangi bir deger olabilir
+        // birinci cozum
 
-        while(age > 0) {
+        int age;
+        do{
             System.out.println("How old are you?");
             age = scanner.nextInt();
 
-//            if(13 > age && 0 < age) {
+//          if(13 > age && 0 < age) su sekilde yazim readibility yi dusuruyor. Tercih etmemeli
             if(0 < age && age < 13) {
 
                 System.out.println("This movie contains violence and not appropriate for you.");
@@ -27,5 +28,32 @@ public class assignment_1 {
                 System.out.println("This movie is appropriate for you.");
             }
         }
-    }
+        while(age > 0); {}
+
+
+        // ikinci cozum
+
+//        int age = 1; // while loop a girmesi icin age 0 dan buyuk herhangi bir deger olabilir
+//
+//        while(age > 0) {
+//            System.out.println("How old are you?");
+//            age = scanner.nextInt();
+//
+////            if(13 > age && 0 < age) {
+//            if(0 < age && age < 13) {
+//
+//                System.out.println("This movie contains violence and not appropriate for you.");
+//            }
+//
+//            else if(13 <= age && age < 18) {
+//                System.out.println("This movie contains nudity and not appropriate for you.");
+//            }
+//
+//            else if (18 <= age) {
+//                System.out.println("This movie is appropriate for you.");
+//            }
+//        }
+
+
+        }
 }
